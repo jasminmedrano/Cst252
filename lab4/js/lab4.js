a0b5c909d5b8343ab31cc49fb4954ccc573652f3
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-02-08 15:44:55 -0800

    Update lab4.js

---
+++ lab4/js/lab4.js
@@ -0,0 +1,21 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+//Wake up
+  //Get out of bed
+    //Go brush teeth with toothpaste and water
+      //Go take a shower
+      //Change into clean clothes
+    //Make yourself a morning breakfast
+  //Have breakfast
+//Clean dishes
