7d1eeb25b87e11c9c2588ae18e2456246e3ff8af
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-02-10 19:16:20 -0800

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -10,8 +10,11 @@ model = "Patriot";
 color = "Red";
 year = 2020;
 
+
 // calculate
 age = 2022 - 2010;
+
+
 
 // output
 document.writeln("Make: " + make + "<br>");
@@ -19,3 +22,21 @@ document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
 document.writeln("Age: " + age + "<br>");
+
+
+
+// Define Variables 2 
+make = "Toyota";
+model = "Tacoma";
+color = "White";
+year = 2020;
+
+// calculate 2
+ age = 2022 - 2019;
+
+ // output 2
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
