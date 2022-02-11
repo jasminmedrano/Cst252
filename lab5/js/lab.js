dee401f548f97243d7fcdef8ae2f09a368ed7cc5
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-02-10 18:35:25 -0800

    11

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Jasmin Medrano <jasmedrano@csumb.edu> & Anthony Lopez-Zamora <anlopezzamora@csumb.edu>
+ * Created:   02.10.2022
+ * License:   Public Domain
+ */
+
+// Define Variables
+make = "Jeep";
+model = "Patriot";
+color = "Red";
+year = 2020;
+
+// calculate
+age = 2022 - 2010;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
