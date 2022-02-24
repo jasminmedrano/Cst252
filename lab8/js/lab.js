83a07c95870e0bb08f915470c8d33069397cdc9a
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-02-24 14:24:53 -0800

    11

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -5,28 +5,22 @@
  */
 
  function aBetterNumSort(x) {
-   return x + 23;
+   return (x + 23);
  }
 
  // test function
  console.log("aBetterNumSort", aBetterNumSort(40));
  console.log("aBetterNumSort", aBetterNumSort(10));
 
-array = [ 10, 40, 567, 2083, 38, 1.5, 69];
-console.log("My array" array);
+array = [ 10, 40, 567, 2083, 38, 1.5, 69]
+console.log("My array", array);
 
 var result = array.map(aBetterNumSort);
 // should return [33, 63, 590, 2106, 61, 24.5, 92]
-console.log("Addition", result);
-
-var result = array.map(function(x) {
-  return x +23;
-})
-// should return [33, 63, 590, 2106. 61, 24,5, 92]
-console.log("Addition Answer", result);
+console.log("Addition of Array", result);
 
 var result = array.map(function(x){
   return x * 2;
 })
 // should return [20, 80, 1134, 4166, 76, 3, 138]
-console.log("Answer:", result);
+console.log("Multiply of Array:", result);
