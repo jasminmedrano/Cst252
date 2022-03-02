9c6f37fa4dd7ae05d30445c288019a9ba60c2ceb
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-01 19:25:43 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,6 +3,35 @@
  * Created:   03. 01.2022
  * License:   Public Domain
  */
+
+var outputEl = getElementById("button")
+
+
+
+
+
+var oneEl = document.createElement("button")
+outputEl.appendChild(oneEl)
+oneEl.innerHTML = "Click here";
+oneEl.style.fontsize = "20px";
+oneEl.style.backgroundColor = "green"
+oneEl.style.color = "white";
+
+
+
+
+var twoEl = document.createElement("button")
+outputEl.appendChild(twoEl)
+oneEl.innerHTML = "Press me!";
+oneEl.style.fontsize = "20px";
+oneEl.style.backgroundColor = "purple"
+oneEl.style.color = "white"
+
+
+
+
+
+
 oneEl.innerHTML = "button text here"; 'here as well'
 oneEl.style.fontsize = "insert size here"; 'here as well'
 oneEl.style.backgroundColor = "insert color here"
