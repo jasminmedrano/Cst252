55c71c8669a278517f9ffc7831dc4a0ff7ac9dce
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-01 20:13:55 -0800

    kj

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -12,9 +12,9 @@ var outputEl = document.getElementById("output");
 //creates a blue "Click here" button for oneEl
 var oneEl = document.createElement("button");
 outputEl.appendChild(oneEl)
-oneEl.innerHTML = "Click here";
+oneEl.innerHTML = "Click here!";
 oneEl.style.fontsize = "20px";
-oneEl.style.backgroundColor = "blue";
+oneEl.style.backgroundColor = "pink";
 oneEl.style.color = "white";
 
 
@@ -22,7 +22,7 @@ oneEl.style.color = "white";
 //creates a purple "Press me!" button for twoEl
 var twoEl = document.createElement("button");
 outputEl.appendChild(twoEl)
-twoEl.innerHTML = "Press me!";
+twoEl.innerHTML = "No click here!";
 twoEl.style.fontsize = "20px";
 twoEl.style.backgroundColor = "purple";
 twoEl.style.color = "white"
