9f853587b6c195daa8b02924df13dc554b7414b8
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-01 17:48:00 -0800

    11

---
+++ lab9/js/lab.js
@@ -0,0 +1,7 @@
+/*
+ * Author:    Jasmin Medrano & Anthony Lopez-Zamora <jasmedrano@csumb.edu> & Anthony Lopez-Zamora <anlopezzamora@csumb.edu>
+ * Created:   03. 01.2022
+ * License:   Public Domain
+ */
+
+newEl.style.backgroundColor = ""
