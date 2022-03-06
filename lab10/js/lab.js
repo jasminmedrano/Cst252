3b8e0e07e2e6a72da3edd4321185b99dcfae371a
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-06 15:18:53 -0800

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,52 +5,23 @@
  */
 
  // sortUserName
- //  a function that takes user input and sorts the letters
- //  of their name
- function sortUserName(userName) {
-     // // convert to lower case
-    userName = userName.toLowerCase();
-    console.log("userName =", userName);
-     // // split string to array
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
-     // // sort the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameArraySort =", nameArraySort);
-     // // join array back to a string
-   var nameSorted = nameArraySort.join('');
-   console.log("nameSorted =", nameSorted);
-     // // Note that I could have done the above lines as a single line:
-   userName.toLowerCase().split("").sort().join("")
-   return userName.toLowerCase().split("").sort().join("");
- }
-
- // sortUserName - a function that takes user input and sorts the letters
- // of their name
- function randomizeName(userName) {
-   // convert userName string to an array
-     var nameArray = userName.toLowerCase().split("");
-     console.log("nameArray =", nameArray);
-  // shuffle array with our shuffle function
-     var shuffledArray = shuffleArray(nameArray);
-     console.log("shuffledArray =", shuffledArray);
-     var shuffledString = shuffledArray.join("");
- // shift to Title Case (like a name)
-     var newName = toTitleCase(shuffledString);
-  // return array to a string
-     return newName;
- }
+  // a function that takes user input and sorts the letters
+  // of their name
+   function sortUserName (){
+          var userName = ("user-name");
+            console.log("userName=",userName);
+      // split string to array
+            var nameArray = userName.split('');
+            console.log("nameArray =", nameArray);
+            var nameArraySort = nameArray.sort();
+            console.log ("nameArraySort =", nameArraySort);
+      // join array back to string
+            var nameSorted = nameArraySort.join('').toUpperCase();
+            console.log ("nameSorted =", nameSorted);
+            return nameSorted;
+  }
 
 
- function main() {
-    var userName = getName();
-     document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
-    var sortedName = sortUserName(userName);
-     document.writeln("Here's your sorted name: " + sortedName + "</br></br>");
-    var randomName = randomizeName(userName);
-     document.writeln("And oh hey, I fixed your name.<br>Your new one is above.");
-     document.writeln("<div class='name'>" + randomName + "</div>")
- }
 
 
 
