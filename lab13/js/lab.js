64b7f8efb01b6b05f7b46246f6ed101031d2068a
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-15 18:30:04 -0700

    ggd

---
+++ lab13/js/lab.js
@@ -0,0 +1,33 @@
+/*
+ * Author:    Jasmin Medrano & Anthony Lopez-Zamora <jasmedrano@csumb.edu> & Anthony Lopez-Zamora <anlopezzamora@csumb.edu>
+ * Created:   03.15.2022
+ * License:   Public Domain
+ */
+
+
+function sortingTeams(name) {
+   // get length of name
+   var len = name.length;
+   // do a mod 3 to get teams
+   var mod = len % 3;
+   // if conditional to set teamsStr to house
+   var teamsStr;
+   if (mod == 0) {
+       teamsStr = "Mystic";
+   } else if (mod == 1) {
+       teamsStr = "Instinct";
+   } else if (mod == 2) {
+      teamsStr = "Valor";
+   }
+   // returnteams txt
+   return teamsStr;
+}
+
+$("#my-button").click(function(){
+
+ var name = $("#input").val()
+ var teams = sortingTeams(name);
+ console.log(teams);
+ $("#output").html("Welcome! Your team is " + teams);
+
+});
