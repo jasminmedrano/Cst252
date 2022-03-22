686fd926e845ed14e2da880c06e2fb7b7c364cd4
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-03-22 16:13:17 -0700

    kjk

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,50 +4,57 @@
  * License:   Public Domain
  */
 
- // sortUserName
-  // a function that takes user input and sorts the letters
-  // of their name
-   function shuffleArray(array) {
-     var currentIndex = array.lenght, tempporaryValue, randomIndex;
 
+ // shuffleArray //
+ function shuffleArray(array) {
+     var currentIndex = array.length, temporaryValue, randomIndex;
+     // Shuffle elements
      while (0 !== currentIndex) {
+         // Pick a remaining element...
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
 
-       randomIndex = Math.floor(Math.random() * currentIndex);
-       currentIndex -= 1;
-
-       tempporaryValue = array[currentIndex];
-       array[currentIndex] = array[randomIndex];
-       array[randomIndex] = tempporaryValue;
-       }
+         // And swap it with the current element.
+         temporaryValue = array[currentIndex];
+         array[currentIndex] = array[randomIndex];
+         array[randomIndex] = temporaryValue;
+     }
      return array;
-   }
+ }
 
+ // sortUserName //
 function reorderUserName(word) {
-  var wordArray = word.toLowerCase().split('');
-  var newArray = shuffleArray(wordArray);
-  return newArray.join('');
+    var wordArray = word.toLowerCase().split('');
+    var newArray = shuffleArray(wordArray);
+    return newArray.join('');
 }
 
+// given a string, return string in Title Case //
 function toTitleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
 }
 
-buttonEl = document.getElementById('my-button');
+// find the button element
+buttonEl = document.getElementById("my-button");
 console.log("button element:", buttonEl);
-
+// find the form element
 inputEl = document.getElementById("user-name");
 console.log("input element:", inputEl);
-
+// find output element
 outputEl = document.getElementById("output");
-console.log("output element:", outputEl)
+console.log("output element:", outputEl);
 
-buttonEl.addEventListener("click", function (){
+// add an event listener to button
+buttonEl.addEventListener("click", function(){
+  // get value from name element
   var userName = inputEl.value;
+  // modify value - either sort or shuffle
   var newName = toTitleCase(reorderUserName(userName));
+  // put value in output element
   outputEl.innerHTML = "<p id=name-results>" + newName + "</p>";
 });
