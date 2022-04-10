6ce7cd4d466a3eb3d06ad7fdaf05f595ba15d1dd
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-04-10 16:58:46 -0700

    xcvd

--- lab17/js/lab.js
+++ lab17/js/lab.js
@@ -5,24 +5,25 @@
  */
 
 
-class Vehicle {
-   constructor(make, model, year, color, extras, name) {
-     this.make = make;
-     this.model = model;
-     this.year = year;
-     this.color = color;
-     this.extras = extras;
-     this.name = name;
-     this.info = function() {
-          return ("It was a " + this.make + " " + this.model + " " + this.year + " " + this.color + " with " + this.extras + " named " + this.name);
-    }
-}
-}
+
+ class Vehicle {
+    constructor(make, model, year, color, extras, name) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.color = color;
+      this.extras = extras;
+      this.name = name;
+      this.info = function() {
+           return ("It was a " + this.make + " " + this.model + " " + this.year + " " + this.color + " with " + this.extras + " named " + this.name);
+     }
+ }
+ }
 
 
-var vehicle1 = new Vehicle("Toyota", "Tacoma", 2019, "white", "4x4", "Gangsta");
-var vehicle2 = new Vehicle("Jeep", "Patriot", 2010, "red", "emergency plushies inside", "I'm in my mums' car! Vroom Vroom");
+ var vehicle1 = new Vehicle("Toyota", "Tacoma", 2019, "white", "4x4", "Gangsta");
+ var vehicle2 = new Vehicle("Jeep", "Patriot", 2010, "red", "emergency plushies inside", "I'm in my mums' car! Vroom Vroom");
 
 
-  $("#output").append("<p>" + vehicle1.info() + "</p>");
-  $("#output").append("<p>" + vehicle2.info() + "</p>");
+   $("#output").append("<p>" + vehicle1.info() + "</p>");
+   $("#output").append("<p>" + vehicle2.info() + "</p>");
