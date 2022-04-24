6ee925828ff6f7d8a2e09f30f2482d36ce59ec62
jasminmedrano <78002503+jasminmedrano@users.noreply.github.com>
2022-04-24 14:15:29 -0700

    jkh

---
+++ lab19/js/lab.js
@@ -0,0 +1,5 @@
+/*
+ * Author:    Jasmin Medrano & Anthony Lopez-Zamora <jasmedrano@csumb.edu> & Anthony Lopez-Zamora <anlopezzamora@csumb.edu>
+ * Created:   04.12.2022
+ * License:   Public Domain
+ */
